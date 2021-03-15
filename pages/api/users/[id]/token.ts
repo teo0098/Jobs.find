d6de99f66e088c67dc1bde4aeee92193d6e53208
Teodor Tkaczyk <teodor.tkaczyk0407@gmail.com>
2021-03-15 20:01:35 +0100

    Alter generating cookies when access token has expired

--- pages/api/users/[id]/token.ts
+++ pages/api/users/[id]/token.ts
@@ -1,9 +1,9 @@
 import { sign } from 'jsonwebtoken'
 import { NextApiRequest, NextApiResponse } from 'next'
+import { serialize } from 'cookie'
 
 import InfoTypes from '../../../../utils/info/InfoTypes'
 import authUser from '../../../../utils/middlewares/authUser'
-import generateCookies from '../../../../utils/middlewares/generateCookies'
 
 const token = async (req : NextApiRequest, res : NextApiResponse) => {
     const { method, cookies, query } = req
@@ -14,7 +14,15 @@ const token = async (req : NextApiRequest, res : NextApiResponse) => {
             if (!user) return res.status(403).json(InfoTypes.WRONG_CREDENTIALS)
             if (!user.refreshTokens.includes(cookies['refreshToken'])) return res.status(403).json(InfoTypes.WRONG_CREDENTIALS)
             const accessToken = sign({ user: user._id }, `${process.env.ACCESS_TOKEN_SECRET}`, { expiresIn: '10m' })
-            generateCookies(res, user.name, user._id, accessToken, cookies['refreshToken'])
+            res.setHeader('Set-Cookie', 
+                serialize('accessToken', accessToken, {
+                    path: '/',
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: 'strict',
+                    maxAge: 60 * 10 // 10 mins
+                })
+            )
             res.status(200).json('Access token has been refreshed successfully')
         }
         catch {
